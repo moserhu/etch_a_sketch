9e911338cd6f6069e02a8d34d89057644849ec41
moserhu <moserhu@gmail.com>
2022-09-26 12:37:12 -0500

    Add final UI and CSS

--- java.js
+++ java.js
@@ -10,6 +10,16 @@ function setCurrentColor(newColor) {
     currentColor = newColor
 }
 
+function setCurrentMode(newMode) {
+    activateButton(newMode)
+    currentMode = newMode
+  }
+  
+function setCurrentSize(newSize) {
+    currentSize = newSize
+  }
+
+
 const colorPicker = document.getElementById('colorPicker')
 const colorBtn = document.getElementById('colorBtn')
 const rainbowBtn = document.getElementById('rainbowBtn')
@@ -19,6 +29,36 @@ const sizeValue = document.getElementById('sizeValue')
 const sizeSlider = document.getElementById('sizeSlider')
 const grid = document.getElementById('grid')
 
+colorPicker.oninput = (e) => setCurrentColor(e.target.value)
+colorBtn.onclick = () => setCurrentMode('color')
+rainbowBtn.onclick = () => setCurrentMode('rainbow')
+eraserBtn.onclick = () => setCurrentMode('eraser')
+clearBtn.onclick = () => reloadGrid()
+sizeSlider.onmousemove = (e) => updateSizeValue(e.target.value)
+sizeSlider.onchange = (e) => changeSize(e.target.value)
+
+let mouseDown = false
+document.body.onmousedown = () => (mouseDown = true)
+document.body.onmouseup = () => (mouseDown = false)
+
+function changeSize(value) {
+    setCurrentSize(value)
+    updateSizeValue(value)
+    reloadGrid()
+  }
+
+function updateSizeValue(value) {
+    sizeValue.innerHTML = `${value} x ${value}`
+  }
+
+function reloadGrid() {
+    clearGrid()
+    setupGrid(currentSize)
+  }
+  
+function clearGrid() {
+    grid.innerHTML = ''
+  }
 
 function setupGrid(size) {
     grid.style.gridTemplateColumns = `repeat(${size}, 1fr)`
@@ -27,12 +67,46 @@ function setupGrid(size) {
     for (let i = 0; i < size * size; i++) {
       const gridElement = document.createElement('div')
       gridElement.classList.add('grid-element')
+      gridElement.addEventListener('mouseover', changeColor)
+        gridElement.addEventListener('mousedown', changeColor)
       grid.appendChild(gridElement)
     }
   }
 
+function changeColor(e) {
+    if (e.type === 'mouseover' && !mouseDown) return
+    if (currentMode === 'rainbow') {
+      const randomR = Math.floor(Math.random() * 256)
+      const randomG = Math.floor(Math.random() * 256)
+      const randomB = Math.floor(Math.random() * 256)
+      e.target.style.backgroundColor = `rgb(${randomR}, ${randomG}, ${randomB})`
+    } else if (currentMode === 'color') {
+      e.target.style.backgroundColor = currentColor
+    } else if (currentMode === 'eraser') {
+      e.target.style.backgroundColor = '#fefefe'
+    }
+  }
+
+  function activateButton(newMode) {
+    if (currentMode === 'rainbow') {
+      rainbowBtn.classList.remove('active')
+    } else if (currentMode === 'color') {
+      colorBtn.classList.remove('active')
+    } else if (currentMode === 'eraser') {
+      eraserBtn.classList.remove('active')
+    }
+  
+    if (newMode === 'rainbow') {
+      rainbowBtn.classList.add('active')
+    } else if (newMode === 'color') {
+      colorBtn.classList.add('active')
+    } else if (newMode === 'eraser') {
+      eraserBtn.classList.add('active')
+    }
+  }
 
 
   window.onload = () => {
     setupGrid(DEFAULT_SIZE)
+    activateButton(DEFAULT_MODE)
   }
